799be48296e0e6f24fd646af1afd64473a9a4aff
Vincent Bernardoff <vb@luminar.eu.org>
2019-10-06 17:31:54 +0800

    fix compilation (fastws)

--- src/coinbasepro_ws_async.ml
+++ src/coinbasepro_ws_async.ml
@@ -12,35 +12,34 @@ module Log = (val Logs.src_log src : Logs.LOG)
 
 let connect ?(sandbox=false) () =
   let url = if sandbox then sandbox_url else url in
-  Fastws_async.connect_ez url >>|
-  Result.map ~f:begin fun (r, w, cleaned_up) ->
-    let client_read = Pipe.map r ~f:begin fun msg ->
-        Ezjsonm_encoding.destruct_safe encoding (Ezjsonm.from_string msg)
-      end in
-    let ws_read, client_write = Pipe.create () in
-    don't_wait_for
-      (Pipe.closed client_write >>| fun () -> Pipe.close w) ;
-    don't_wait_for @@
-    Pipe.transfer ws_read w ~f:begin fun cmd ->
-      let doc =
-        match Ezjsonm_encoding.construct encoding cmd with
-        | `A _ | `O _ as a -> Ezjsonm.to_string a
-        | _ -> invalid_arg "not a json document" in
-      Log.debug (fun m -> m "-> %s" doc) ;
-      doc
-    end ;
-    (client_read, client_write, cleaned_up)
-  end
+  Deferred.Or_error.map (Fastws_async.EZ.connect url)
+    ~f:begin fun { r; w; cleaned_up } ->
+      let client_read = Pipe.map r ~f:begin fun msg ->
+          Ezjsonm_encoding.destruct_safe encoding (Ezjsonm.from_string msg)
+        end in
+      let ws_read, client_write = Pipe.create () in
+      don't_wait_for
+        (Pipe.closed client_write >>| fun () -> Pipe.close w) ;
+      don't_wait_for @@
+      Pipe.transfer ws_read w ~f:begin fun cmd ->
+        let doc =
+          match Ezjsonm_encoding.construct encoding cmd with
+          | `A _ | `O _ as a -> Ezjsonm.to_string a
+          | _ -> invalid_arg "not a json document" in
+        Log.debug (fun m -> m "-> %s" doc) ;
+        doc
+      end ;
+      (client_read, client_write, cleaned_up)
+    end
 
 let connect_exn ?sandbox () =
   connect ?sandbox () >>= function
-  | Error `Internal exn -> raise exn
-  | Error `WS e -> Fastws_async.raise_error e
+  | Error e -> Error.raise e
   | Ok a -> return a
 
 let with_connection ?(sandbox=false) f =
   let url = if sandbox then sandbox_url else url in
-  Fastws_async.with_connection_ez url ~f:begin fun r w ->
+  Fastws_async.EZ.with_connection url ~f:begin fun r w ->
     let client_read = Pipe.map r ~f:begin fun msg ->
         Ezjsonm_encoding.destruct_safe encoding (Ezjsonm.from_string msg)
       end in
@@ -59,7 +58,5 @@ let with_connection ?(sandbox=false) f =
 
 let with_connection_exn ?sandbox f =
   with_connection ?sandbox f >>= function
-  | Error `Internal exn -> raise exn
-  | Error `User_callback exn -> raise exn
-  | Error `WS e -> Fastws_async.raise_error e
+  | Error e -> Error.raise e
   | Ok a -> return a
--- src/coinbasepro_ws_async.mli
+++ src/coinbasepro_ws_async.mli
@@ -2,18 +2,18 @@ open Async
 
 open Coinbasepro_ws
 
+val url : Uri.t
+val sandbox_url : Uri.t
+
 val connect : ?sandbox:bool -> unit ->
-  (t Pipe.Reader.t * t Pipe.Writer.t * unit Deferred.t,
-   [ `Internal of exn | `WS of Fastws_async.error ]) result Deferred.t
+  (t Pipe.Reader.t * t Pipe.Writer.t * unit Deferred.t) Deferred.Or_error.t
 
 val connect_exn : ?sandbox:bool -> unit ->
   (t Pipe.Reader.t * t Pipe.Writer.t * unit Deferred.t) Deferred.t
 
 val with_connection : ?sandbox:bool ->
   (t Pipe.Reader.t -> t Pipe.Writer.t -> 'a Deferred.t) ->
-  ('a, [ `Internal of exn
-       | `User_callback of exn
-       | `WS of Fastws_async.error ]) result Deferred.t
+  'a Deferred.Or_error.t
 
 val with_connection_exn : ?sandbox:bool ->
   (t Pipe.Reader.t -> t Pipe.Writer.t -> 'a Deferred.t) -> 'a Deferred.t
